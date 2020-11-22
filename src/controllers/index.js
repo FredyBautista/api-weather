2f5f8c0a00cb6529d35954f991d49e682079caac
Fredy Bautista <UNOSQUARE\fredy.bautista@sao-lt-190625a.ad.unosquare.com>
2020-11-21 20:02:11 -0600

    ep to get the forecast

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,9 @@
 const getGreeting = require("./greeting");
 const getWeather = require("./weather");
+const getForecast = require("./forecast");
 
 module.exports = {
   getGreeting,
   getWeather,
+  getForecast,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const { getGreeting, getWeather } = require("../controllers");
+const { getGreeting, getWeather, getForecast } = require("../controllers");
 
 router.get("/greeting", getGreeting);
 router.get("/weather/:city", getWeather);
+router.get("/forecast", getForecast);
 
 module.exports = router;
