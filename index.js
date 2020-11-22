c928dab16042c997726247fce3e69b0fb5311733
Fredy Bautista <UNOSQUARE\fredy.bautista@sao-lt-190625a.ad.unosquare.com>
2020-11-21 20:30:07 -0600

    change port

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
 const router = require("./src/routes");
 const errorHandler = require("./src/utils/errorHandler");
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use("/api", router);
 app.use(errorHandler);
-app.listen(3010, () => console.log("The server started on port 3010"));
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cool = require('cool-ascii-faces');
 const router = express.Router();
 const { getGreeting, getWeather, getForecast } = require("../controllers");
 
 router.get("/greeting", getGreeting);
 router.get("/weather/:city", getWeather);
 router.get("/forecast", getForecast);
+router.get('/cool', (req, res) => res.send(cool()))
 
 module.exports = router;
